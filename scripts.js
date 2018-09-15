95e64ffb4218c574aa475c2cfefa345d4e889f9a
bejo <bejoann@gmail.com>
2018-09-15 17:02:24 +1000

    add new functionality and assets

--- scripts.js
+++ scripts.js
@@ -28,6 +28,35 @@ $(document).ready(function(){
     });
 });
 
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel({interval: 500});
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+
+
 var modal = document.getElementById('myModal');
 var btn = document.getElementById('myBtn');
 var span = document.getElementsByClassName("close")[0];
